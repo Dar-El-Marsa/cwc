c84a70635d4c48d8728fc916dcd9dd42d5bf7cf7
jemacom <jemacomchokri@gmail.com>
2015-02-15 16:41:09 +0100

    Add CRUD Employee

--- src/main/java/com/esprit/cwc/services/EmployeesServices.java
+++ src/main/java/com/esprit/cwc/services/EmployeesServices.java
@@ -1,11 +1,16 @@
 package com.esprit.cwc.services;
 
-import javax.ejb.Remote;
+import java.util.List;
 
+import javax.ejb.Remote;
 import com.esprit.cwc.entities.Employee;
 
 
 @Remote
 public interface EmployeesServices {
-	public Employee findById(Employee emp);
+	public Employee findById(int id);
+	public boolean add(Employee employee);
+	public boolean remove(Employee employee);
+	public boolean update(Employee employee);
+	public List<Employee> findAll();
 }
--- src/main/java/com/esprit/cwc/services/EmployeesServicesImpl.java
+++ src/main/java/com/esprit/cwc/services/EmployeesServicesImpl.java
@@ -1,8 +1,10 @@
 package com.esprit.cwc.services;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.*;
+
 import com.esprit.cwc.entities.Employee;
 
 @Stateless
@@ -12,9 +14,45 @@ public class EmployeesServicesImpl implements EmployeesServices {
 	EntityManager manager;
 	
 	@Override
-	public Employee findById(Employee emp) {
-		return manager.find(Employee.class, emp.getId());
+	public Employee findById(int id) {
+		return manager.find(Employee.class, id);
 		
+	}
+
+	@Override
+	public boolean add(Employee employee) {
+		try {
+			manager.persist(employee);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}	
+	}
+
+	@Override
+	public boolean remove(Employee employee) {
+		try {
+			manager.remove(manager.merge(employee));
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+	}
+
+	@Override
+	public boolean update(Employee employee) {
+		try {
+			manager.merge(employee);
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+	}
+
+	@Override
+	public List<Employee> findAll() {
+		Query query = manager.createQuery("select u from Employee u");
+		return query.getResultList();
 	}
 
 }
